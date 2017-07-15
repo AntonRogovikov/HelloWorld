99c5db8f98aee86b5b3fd342d949c235807059ef
Anton Rogovikov <rogovikov2016@gmail.com>
2017-07-15 21:48:55 +0300

    Создан java-файл

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Anton Rogovikov on 15.07.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
